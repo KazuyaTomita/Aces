40bc57c586c62728a4a1d0cf68c45d8b96520953
Kazuya Tomita <iamtommy1125@icloud.com>
2019-03-04 20:04:26 +0900

    add basic routines

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
-package Aces
+package main
 
 import "fmt"
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Printf("start\n")
+	for {
+		search()
+		choose_action()
+		end_game()
+	}
+
 }
+
+func search() {
+	fmt.Printf("search starts\n")
+	// execute search and set next action to global variables
+}
+
+func choose_action() {
+	fmt.Printf("choose next action from \n")
+}
+
+func end_game() {
+	fmt.Printf("Dose this game end? \n")
+}
+
