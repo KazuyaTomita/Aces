f9cebcf2945dfa68e35deb21ded2d68000e69431
Kazuya Tomita <iamtommy1125@icloud.com>
2019-03-30 21:04:39 +0900

    add treePolicy

--- search.go
+++ search.go
@@ -5,25 +5,18 @@ See http://mcts.ai/pubs/mcts-survey-master.pdf for details.
  */
 package main
 
+const Ucb1Coeficient = 0.5
+
 type Search struct {
 	// position index from button. ex) SB is 1, BB is 2.
 	RootNode Node
 }
 
-type Node struct {
-	State State
-	Children []Node
-	Parent Node
-}
-
-func (node *Node) getActionFor(nextNode *Node) Action {
-	return Action{}
-}
-
 func (s *Search) UCB1Search(state *State) Action {
 	node := state.toNode()
+	// how many searches are executed is option
+	// it will be different in both learning and playing case
 	for i := 0; i < 1600; i++ {
-		// iの値が0から9まで変化していく
 		nextNode := treePolicy(&node)
 		reward := defaultPolicy(&node)
 		backup(reward, &nextNode)
@@ -33,10 +26,23 @@ func (s *Search) UCB1Search(state *State) Action {
 }
 
 func treePolicy(node *Node) Node {
+	for !node.isTerminal() {
+		if !node.isExpanded() {
+			return expand(node)
+		}
+		newNode := bestChild(node, Ucb1Coeficient)
+		node = &newNode
+	}
 	// go into tree following UCB1 formula
 	// just go into already an expanded-tree or expand a new node.
+	return *node
+}
+
+func expand(node *Node) Node {
+	// return next node
 	return Node{}
 }
+
 
 func defaultPolicy(node *Node) int {
 	// do play out
